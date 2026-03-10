fbdfbdaf91c5175f1010ad9ce0240627019da03b
NKG <naka1028koki@gmail.com>
2026-03-10 16:25:46 +0900

    Add files via upload

---
+++ Scripts/Sound/AudioClip.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+/// <summary>
+/// BGM‚Ì‰¹Œ¹‚ğİ’è
+/// </summary>
+[Serializable]
+public enum BGMSource
+{
+    STAGE_BGM,
+    GAME_CLEAR_BGM,
+    GAME_OVER_BGM
+}
+/// <summary>
+/// SE‚Ì‰¹Œ¹‚ğİ’è
+/// </summary>
+[Serializable]
+public enum SESource
+{
+    HAPPY
+}
