678022f3d119adf53b84d228fa74c0304155b168
NKG <naka1028koki@gmail.com>
2026-04-24 00:48:08 +0900

    Add files via upload

--- Scripts/Stage/Player/CameraFollow.cs
+++ Scripts/Stage/Player/CameraFollow.cs
@@ -1,20 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// カメラ追従の処理を行うクラス
+/// </summary>
 public class CameraFollow : MonoBehaviour
 {
+    [Header("プレイヤー参照")]
+    [Tooltip("プレイヤーの位置を取得し、カメラの位置を更新する")]
     [SerializeField] private Transform playerTransform;
-    private float playerOffset = -10f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float playerZOffset = -10f;
 
-    // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(playerTransform.position.x, 0f, playerOffset);
+        //常にプレイヤーの位置に合わせてカメラの位置を更新する
+        transform.position = new Vector3(playerTransform.position.x, 0f, playerZOffset);
     }
 }
