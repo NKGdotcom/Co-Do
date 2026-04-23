b6ccd1868c2dd6bbad3c550cf63dc12b56029aa2
NKG <naka1028koki@gmail.com>
2026-04-24 00:48:53 +0900

    Add files via upload

--- Scripts/Stage/Task/Interface/ITaskController.cs
+++ Scripts/Stage/Task/Interface/ITaskController.cs
@@ -1,16 +1,17 @@
+using Cysharp.Threading.Tasks;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// タスクに必要な要素
+/// タスクに必要な要素を定義するインターフェース
 /// </summary>
 public interface ITaskController
 {
     Item[] NeedItems { get; } //必要なアイテムについて
     bool CanExcuteTask(Item _playerHasItem); //タスクが完了できるかどうか
-    void ExcuteTask(); //タスク完了したら
+    UniTaskVoid ExcuteTaskAsync(); //タスク完了したら
 
     event Action OnCompleteTask; //タスクが完了したと伝える
 }
--- Scripts/Stage/Task/Interface/ITaskView.cs
+++ Scripts/Stage/Task/Interface/ITaskView.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// タスクのコメントを定義するインタフェース
+/// </summary>
 public interface ITaskView
 {
     GameObject CommentUI { get; } //コメントUI
