db174b326682fd9d37901ed7a0f3e746b67f3cfb
NKG <naka1028koki@gmail.com>
2026-04-24 00:44:15 +0900

    Add files via upload

--- Scripts/Stage/Item/Interface/IItem.cs
+++ Scripts/Stage/Item/Interface/IItem.cs
@@ -2,7 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// アイテムを定義するインタフェース
+/// </summary>
 public interface IItem 
 {
+    /// <summary>
+    /// 自分の現在持っているアイテムを返すプロパティ
+    /// </summary>
     Item MyItem {  get; }
 }
